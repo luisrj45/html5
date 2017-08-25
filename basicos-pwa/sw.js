6bddce85a1beffd25f00f9b40de7d46ccb07965a
Jonathan MirCha <jonmircha@gmail.com>
2017-08-25 11:31:25 -0500

    Documentando Service Worker para clase 4

--- basicos-pwa/sw.js
+++ basicos-pwa/sw.js
@@ -1,14 +1,13 @@
 const CACHE_NAME = 'pwa-demo-edteam-cache-v1',
   urlsToCache = [
-    '/',
     './',
     './?utm=homescreen',
     './index.html',
     './index.html?utm=homescreen',
     './style.css',
     './script.js',
-    './sw.js',
     './favicon.ico',
+    './img/icon_192x192.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
   ]
 
@@ -19,6 +18,8 @@ self.addEventListener('install', e => {
       .then(cache => {
         console.log('Archivos en cache')
         return cache.addAll(urlsToCache)
+        .then( () => self.skipWaiting() )
+        //skipWaiting forza al SW a activarse
       })
       .catch(err => console.log('Falló registro de cache', err) )
   )
@@ -32,14 +33,15 @@ self.addEventListener('activate', e => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if ( cacheWhitelist.indexOf(cacheName) === -1 ) {
+          //Eliminamos lo que ya no se necesita en cache
+          if ( cacheWhitelist.indexOf(cacheName) === -1 )
             return caches.delete(cacheName)
-          }
         })
       )
     })
     .then(() => {
-      console.log('El cache esta depurado y actualizado')
+      console.log('Cache actualizado')
+      // Le indica al SW activar el cache actual
       return self.clients.claim()
     })
   )
@@ -48,25 +50,22 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   console.log('Evento: SW Recuperando');
   e.respondWith(
-
+    //Miramos si la petición coincide con un elemento del cache
     caches.match(e.request)
       .then(res => {
-        if ( res ) {
-          return res
-        }
-
-        return fetch( request )
-          .then( res => {
-            let resToCache = res.clone()
-            caches.open(cacheName)
+        console.log('Recuperando cache')
+        //Si coincide retornamos el elemento del cache
+        //De lo contrario, solicitamos el elemento a la red
+        return res || fetch( e.request )
+          .then(res => {
+            caches.open(CACHE_NAME)
               .then(cache => {
                 cache
-                  .put(request, resToCache)
-                  .catch( err => console.log(`${request.url}: ${err.message}`) )
+                  .put( e.request, res.clone() )
+                  .catch( err => console.log(`${e.request.url}: ${err.message}`) )
               })
             })
             return res
           })
   )
 })
-
